8c0be77bd5eca936274f869468e5f9f3a84585ea
Luke Watts <sayhello@thisis.la>
2012-10-19 12:16:47 +0100

    Updated title seperator

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 	 */
 	global $page, $paged;
 
-	wp_title( '|', true, 'right' );
+	wp_title( ' &ndash; ', true, 'right' );
 
 	// Add the blog name.
 	bloginfo( 'name' );
@@ -26,11 +26,11 @@
 	// Add the blog description for the home/front page.
 	$site_description = get_bloginfo( 'description', 'display' );
 	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
+		echo " &ndash; $site_description";
 
 	// Add a page number if necessary:
 	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'BCMH' ), max( $paged, $page ) );
+		echo ' &ndash; ' . sprintf( __( 'Page %s', 'BCMH' ), max( $paged, $page ) );
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
